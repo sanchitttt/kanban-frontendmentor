1cd7af43065410bcc10788c64690a89ab2a1384d
Sanchit Tewari <46245135+sanchitttt@users.noreply.github.com>
2023-02-11 07:23:53 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,15 @@
 let routes = {
-    BACKEND_ROUTE: `http://localhost:8082/v1`,
-    SIGNUP_ROUTE: `http://localhost:8082/v1/auth/register`,
-    LOGIN_ROUTE: `http://localhost:8082/v1/auth/login`,
-    DASHBOARD_ROUTE: `http://localhost:8082/v1/dashboard`,
-    EDIT_TASK_ROUTE: `http://localhost:8082/v1/dashboard/editTask`,
-    ADD_TASK_ROUTE: `http://localhost:8082/v1/dashboard/addTask`,
-    EDIT_BOARD_ROUTE: `http://localhost:8082/v1/dashboard/editBoard`,
-    ADD_BOARD_ROUTE: `http://localhost:8082/v1/dashboard/addBoard`,
-    SUBTASKS_PATCH_ROUTE : `http://localhost:8082/v1/dashboard/viewTask`,
-    DELETE_BOARD_ROUTE : `http://localhost:8082/v1/dashboard/deleteBoard`,
-    DELETE_TASK_ROUTE : `http://localhost:8082/v1/dashboard/deleteTask`,
+    BACKEND_ROUTE: `https://kanban-backend-hazel.vercel.app/v1`,
+    SIGNUP_ROUTE: `https://kanban-backend-hazel.vercel.app/v1/auth/register`,
+    LOGIN_ROUTE: `https://kanban-backend-hazel.vercel.app/v1/auth/login`,
+    DASHBOARD_ROUTE: `https://kanban-backend-hazel.vercel.app/v1/dashboard`,
+    EDIT_TASK_ROUTE: `https://kanban-backend-hazel.vercel.app/v1/dashboard/editTask`,
+    ADD_TASK_ROUTE: `https://kanban-backend-hazel.vercel.app/v1/dashboard/addTask`,
+    EDIT_BOARD_ROUTE: `https://kanban-backend-hazel.vercel.app/v1/dashboard/editBoard`,
+    ADD_BOARD_ROUTE: `https://kanban-backend-hazel.vercel.app/v1/dashboard/addBoard`,
+    SUBTASKS_PATCH_ROUTE : `https://kanban-backend-hazel.vercel.app/v1/dashboard/viewTask`,
+    DELETE_BOARD_ROUTE : `https://kanban-backend-hazel.vercel.app/v1/dashboard/deleteBoard`,
+    DELETE_TASK_ROUTE : `https://kanban-backend-hazel.vercel.app/v1/dashboard/deleteTask`,
 }
 
 export default routes;
