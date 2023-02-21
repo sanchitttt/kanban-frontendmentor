5e74874b0da4799e463dc2cbb3307e12ee522aa2
Sanchit Tewari <46245135+sanchitttt@users.noreply.github.com>
2023-02-21 14:13:33 +0530

    Update AllBoardsText.js

--- src/components/AllBoardsText.js
+++ src/components/AllBoardsText.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ModalsContext } from './Dashboard';
 
 function AllBoardsText({count}) {
   return (
     <div className='font-jakarata text-light-grey font text-[12px] font-bold w-[]'>
-        ALL BOARDS ({count})
+        ALL BOARDS ({modals.boardsData.val.boards.length})
     </div>
   )
 }
